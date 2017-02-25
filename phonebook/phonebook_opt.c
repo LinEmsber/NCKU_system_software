8e94a9d8dd9d77bd26af70408e503b045ac6ef0c
LinEmsber <LinEmsber@gmail.com>
2017-02-25 23:40:37 +0800

    small structure to reduece space use

--- phonebook/phonebook_opt.c
+++ phonebook/phonebook_opt.c
@@ -1,15 +1,28 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "phonebook_opt.h"
+
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
-    return NULL;
+
+	while (pHead != NULL) {
+		if (strcasecmp(lastName, pHead->lastName) == 0)
+			return pHead;
+		pHead = pHead->pNext;
+	}
+
+	return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+	e->pNext = (entry *) malloc(sizeof(entry));
+	e = e->pNext;
+	strcpy(e->lastName, lastName);
+	e->pNext = NULL;
+
+	return e;
 }
