aa37e7c2b4aae057656e3c35081d5de488a29e41
LinEmsber <LinEmsber@gmail.com>
2017-02-25 23:39:38 +0800

    add some comments and re-arrange it to my coding style

--- phonebook/main.c
+++ phonebook/main.c
@@ -1,3 +1,5 @@
+/* phone book */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,87 +15,105 @@
 #endif
 
 #define DICT_FILE "./dictionary/words.txt"
+#define DICT_FILE argv[1]
 
 static double diff_in_second(struct timespec t1, struct timespec t2)
 {
-    struct timespec diff;
-    if (t2.tv_nsec-t1.tv_nsec < 0) {
-        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
-        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
-    } else {
-        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
-        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
-    }
-    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+	struct timespec diff;
+	if (t2.tv_nsec-t1.tv_nsec < 0) {
+		diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+	} else {
+		diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+	}
+	return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
 }
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    int i = 0;
-    char line[MAX_LAST_NAME_SIZE];
-    struct timespec start, end;
-    double cpu_time1, cpu_time2;
+	FILE *fp;
+	int i = 0;
+	char line[MAX_LAST_NAME_SIZE];
+	struct timespec start, end;
+	double cpu_time1, cpu_time2;
 
-    /* check file opening */
-    fp = fopen(DICT_FILE, "r");
-    if (fp == NULL) {
-        printf("cannot open the file\n");
-        return -1;
-    }
+	/* check file opening */
+	fp = fopen(DICT_FILE, "r");
+	if (fp == NULL) {
+		printf("cannot open the file\n");
+		return -1;
+	}
 
-    /* build the entry */
-    entry *pHead, *e;
-    pHead = (entry *) malloc(sizeof(entry));
-    printf("size of entry : %lu bytes\n", sizeof(entry));
-    e = pHead;
-    e->pNext = NULL;
+	/* build the entry */
+	entry *pHead, *e;
+	pHead = (entry *) malloc(sizeof(entry));
+	printf("size of entry : %lu bytes\n", sizeof(entry));
+	e = pHead;
+	e->pNext = NULL;
 
-#if defined(__GNUC__)
-    __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
-#endif
-    clock_gettime(CLOCK_REALTIME, &start);
-    while (fgets(line, sizeof(line), fp)) {
-        while (line[i] != '\0')
-            i++;
-        line[i - 1] = '\0';
-        i = 0;
-        e = append(line, e);
-    }
-    clock_gettime(CLOCK_REALTIME, &end);
-    cpu_time1 = diff_in_second(start, end);
-
-    /* close file as soon as possible */
-    fclose(fp);
-
-    e = pHead;
-
-    /* the givn last name to find */
-    char input[MAX_LAST_NAME_SIZE] = "zyxel";
-    e = pHead;
-
-    assert(findName(input, e) &&
-           "Did you implement findName() in " IMPL "?");
-    assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
 
 #if defined(__GNUC__)
-    __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
+	__builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
-    /* compute the execution time */
-    clock_gettime(CLOCK_REALTIME, &start);
-    findName(input, e);
-    clock_gettime(CLOCK_REALTIME, &end);
-    cpu_time2 = diff_in_second(start, end);
 
-    FILE *output = fopen(OUT_FILE, "a");
-    fprintf(output, "append() findName() %lf %lf\n", cpu_time1, cpu_time2);
-    fclose(output);
+	clock_gettime(CLOCK_REALTIME, &start);
 
-    printf("execution time of append() : %lf sec\n", cpu_time1);
-    printf("execution time of findName() : %lf sec\n", cpu_time2);
+	// read file stream from file, read one line at one time.
+	while (fgets(line, sizeof(line), fp)) {
 
-    if (pHead->pNext) free(pHead->pNext);
-    free(pHead);
+		// find the terminate byte, '\0'
+		while (line[i] != '\0')
+			i++;
+		line[i - 1] = '\0';
 
-    return 0;
+		// reset i for caculating the next line
+		i = 0;
+
+		// append this line into linked list
+		e = append(line, e);
+
+	}
+	clock_gettime(CLOCK_REALTIME, &end);
+	cpu_time1 = diff_in_second(start, end);
+
+	/* After we read all content and save those content into linked list,
+	 * close file as soon as possible */
+	fclose(fp);
+
+	e = pHead;
+
+	/* the givn last name to find */
+	char input[MAX_LAST_NAME_SIZE] = "zyxel";
+	e = pHead;
+
+	assert(findName(input, e) && "Did you implement findName() in " IMPL "?");
+	assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
+
+#if defined(__GNUC__)
+	__builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
+#endif
+	/* compute the execution time */
+	clock_gettime(CLOCK_REALTIME, &start);
+	findName(input, e);
+	clock_gettime(CLOCK_REALTIME, &end);
+	cpu_time2 = diff_in_second(start, end);
+
+	FILE *output = fopen(OUT_FILE, "a");
+	fprintf(output, "append() findName() %lf %lf\n", cpu_time1, cpu_time2);
+	fclose(output);
+
+	printf("execution time of append() : %lf sec\n", cpu_time1);
+	printf("execution time of findName() : %lf sec\n", cpu_time2);
+
+	// if (pHead->pNext)
+	// 	free(pHead->pNext);
+
+	while((e = pHead)){
+		pHead = pHead->pNext;
+		free(e);
+	}
+	free(pHead);
+
+	return 0;
 }
