dbfdc78b3b683d5e28b31d757dc2d5f632146229
Lin Emsber <linemsber@gmail.com>
2017-03-05 23:21:10 +0800

    Change source codes to my coding style

--- raytracing/raytracing.h
+++ raytracing/raytracing.h
@@ -4,8 +4,12 @@
 #include "objects.h"
 #include <stdint.h>
 
-void raytracing(uint8_t *pixels, color background_color,
-                rectangular_node rectangulars, sphere_node spheres,
-                light_node lights, const viewpoint *view,
-                int width, int height);
+void raytracing(uint8_t *pixels,
+		color background_color,
+		rectangular_node rectangulars,
+		sphere_node spheres,
+		light_node lights,
+		const viewpoint *view,
+		int width,
+		int height);
 #endif
