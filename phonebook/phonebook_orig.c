bec7f147c4d5f0e151b9d106fd65cf7ba698aee5
Lin Emsber <linemsber@gmail.com>
2017-05-30 22:02:48 +0800

    Check the algorithms and unify the coding style.
    1. Add list_delete_all().
    2. Unify the source code coding style.

--- phonebook/phonebook_orig.c
+++ phonebook/phonebook_orig.c
@@ -8,33 +8,33 @@
 /* original version */
 entry *list_append(char lastName[], entry *e)
 {
-        /* allocate memory for the new entry and put lastName */
-        e->p_next = (entry *) malloc(sizeof(entry));
-        e = e->p_next;
-        strcpy(e->lastName, lastName);
-        e->p_next = NULL;
+	/* allocate memory for the new entry and put lastName */
+	e->p_next = (entry *) malloc(sizeof(entry));
+	e = e->p_next;
+	strcpy(e->lastName, lastName);
+	e->p_next = NULL;
 
-        return e;
+	return e;
 }
 
 
 entry *list_find_name(char lastName[], entry *p_head)
 {
-    while (p_head != NULL) {
-        if (strcasecmp(lastName, p_head->lastName) == 0)
-            return p_head;
-        p_head = p_head->p_next;
-    }
-    return NULL;
+	while (p_head != NULL) {
+		if (strcasecmp(lastName, p_head->lastName) == 0)
+			return p_head;
+		p_head = p_head->p_next;
+	}
+	return NULL;
 }
 
 
 void list_delete_all(entry * p_head)
 {
-        entry * tmp;
-        while(p_head){
-                tmp = p_head;
-                p_head = p_head->p_next;
-                free(tmp);
-        }
+	entry * tmp;
+	while(p_head){
+		tmp = p_head;
+		p_head = p_head->p_next;
+		free(tmp);
+	}
 }
--- phonebook/phonebook_orig.h
+++ phonebook/phonebook_orig.h
@@ -6,17 +6,17 @@
 
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
-    struct __PHONE_BOOK_ENTRY * p_next;
+	char lastName[MAX_LAST_NAME_SIZE];
+	char firstName[16];
+	char email[16];
+	char phone[10];
+	char cell[10];
+	char addr1[16];
+	char addr2[16];
+	char city[16];
+	char state[2];
+	char zip[5];
+	struct __PHONE_BOOK_ENTRY * p_next;
 } entry;
 
 
